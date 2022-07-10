30eb652fdd946d9fe68697968a09406aad947de9
scrymastic <99010732+scrymastic@users.noreply.github.com>
2022-07-10 19:55:25 +0700

    Update run.py

--- balanceChemicalEquation/run.py
+++ balanceChemicalEquation/run.py
@@ -5,24 +5,31 @@ def main():
     """
     This function will calculate the balance of a chemical equation.
     """
-    print("This program will calculate the balance of a chemical equation.")
-    print("Enter the reactants and products of the chemical equation.")
-    print("Each chemical should be separated by a comma.")
-    print("Example:")
-    print("reactants: KMnO4, FeSO4, H2SO4")
-    print("products: MnSO4, Fe2(SO4)3, K2SO4, H2O")
-    print("Now, enter your equation:")
+    continuation = True
+    while continuation:
+        reactants = input("reactants: ")
+        if reactants == "/":
+            continuation = False
+            print("Bye!")
+            quit()
+        products = input("products: ")
 
-    reactants = input("reactants: ")
-    products = input("products: ")
-
-    print(balance(reactants, products))
+        print(balance(reactants, products))
 
     # f = open("testcase.txt", "r")
     # for line in f:
     #     reactants = line.split(' - ')[0]
     #     products = line.split(' - ')[1]
     #     print(balance(reactants, products))
+
+print("This program will calculate the balance of a chemical equation.")
+print("Enter the reactants and products of the chemical equation.")
+print("Each chemical should be separated by a comma.")
+print("Example:")
+print("reactants: KMnO4, FeSO4, H2SO4")
+print("products: MnSO4, Fe2(SO4)3, K2SO4, H2O")
+print("Enter / to quit.")
+print("Now, enter your equation:")
 
 if __name__ == "__main__":
     main()
