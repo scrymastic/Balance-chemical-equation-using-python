a3e7d77082c08f990dcf80c880f4b2b1de72f3ec
scrymastic <99010732+scrymastic@users.noreply.github.com>
2022-07-10 10:27:36 +0700

    Update solve_simul_equation.py

--- balanceChemicalEquation/solve_simul_equation.py
+++ balanceChemicalEquation/solve_simul_equation.py
@@ -1,5 +1,6 @@
 from fractions import Fraction
 from numpy import linalg
+from math import lcm
 
 # solve simultaneous equations with some unknowns
 # example:
@@ -7,17 +8,19 @@ from numpy import linalg
 # x + y + z = 1
 # x + 2y + 3z = 2
 # x + 3y + 6z = 3
-# A = np.array([[1, 1, 1], [1, 2, 3], [1, 3, 6]])
-# Y = np.array([1, 2, 3])
+# A = [[1, 1, 1], [1, 2, 3], [1, 3, 6]]
+# Y = [1, 2, 3]
 # returns: [0, 1, 0]
 def solve(A, Y):
     result = linalg.inv(A).dot(Y)
     # get result as fractions
     result = [Fraction(x).limit_denominator() for x in result]
-    # get max denominator
-    max_denominator = max([x.denominator for x in result])
+    # get lcm of all denominators
+    lcm_of_denominators = lcm(*[x.denominator for x in result])
     # get result as integers
-    result = [int(x * max_denominator) for x in result]
-    result = [1*max_denominator] + result
+    result = [int(x * lcm_of_denominators) for x in result]
+
+    result = [lcm_of_denominators] + result
+  
     return result
 
