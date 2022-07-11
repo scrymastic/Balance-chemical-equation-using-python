6d34175addadfc2bd9bf44bfc2debdc3e66c6b85
scrymastic <99010732+scrymastic@users.noreply.github.com>
2022-07-11 09:40:54 +0700

    Update run.py

--- balanceChemicalEquation/run.py
+++ balanceChemicalEquation/run.py
@@ -1,38 +1,29 @@
 
 from analysis_chemical_equation import *
 
+
 def main():
     """
     This function will calculate the balance of a chemical equation.
     """
-    continuation = True
-    while continuation:
-        reactants = input("reactants: ")
-        if reactants == "/":
-            continuation = False
-            print("Bye!")
-            quit()
-        products = input("products: ")
-        if products == "/":
-            continuation = False
+    while True:
+        chemical_equation = input("Equation: ")
+        if chemical_equation == "/":
             print("Bye!")
             quit()
 
-        print(balance(reactants, products))
+        print(balance(chemical_equation))
 
     # f = open("testcase.txt", "r")
     # for line in f:
-    #     reactants = line.split(' - ')[0]
-    #     products = line.split(' - ')[1]
-    #     print(balance(reactants, products))
+    #     print(balance(line))
 
 print("This program will calculate the balance of a chemical equation.")
-print("Enter the reactants and products of the chemical equation.")
-print("Each chemical should be separated by a comma.")
+print("Enter the chemical equation or / to exit.")
+print("The reactants and products should be separated by a equal sign (=).")
+print("Each chemical should be separated by a plus sign (+).")
 print("Example:")
-print("reactants: KMnO4, FeSO4, H2SO4")
-print("products: MnSO4, Fe2(SO4)3, K2SO4, H2O")
-print("Enter / to quit.")
+print("Equation: KMnO4 + FeSO4 + H2SO4 = MnSO4 + Fe2(SO4)3 + K2SO4 + H2O")
 print("Now, enter your equation:")
 
 if __name__ == "__main__":
